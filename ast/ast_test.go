3555c3eb9b9fefa5592f492beee39bbf4fa81410
radeqq007 <radeqq007@interia.pl>
2024-12-15 16:56:21 +0100

    test: add unit test for ast

---
+++ ast/ast_test.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"testing"
+	"vex-programming-language/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&VarStatement{
+				Token: token.Token{Type: token.VAR, Literal: "var"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVal"},
+					Value: "AnotherVal",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+
+}
