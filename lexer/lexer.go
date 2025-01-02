973e8d0a73685bb14acb7191d4d261a43bf4e2e5
radeqq007 <radeqq007@interia.pl>
2025-01-02 15:58:52 +0100

    feat: lexing >= and <=

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -92,10 +92,22 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.ASTERISK, l.ch)
 	
 	case '<':
-		tok = newToken(token.LT, l.ch)
-	
-	case '>':
-		tok = newToken(token.GT, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.LE, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.LT, l.ch)
+		}
+
+	case '>':	
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.GE, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.GT, l.ch)
+		}
 	
 	case '(':
 			tok = newToken(token.LPAREN, l.ch)
