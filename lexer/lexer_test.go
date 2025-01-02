9d3dc82c1630c81fd289cf0bc50359d565f1540b
radeqq007 <radeqq007@interia.pl>
2025-01-02 16:20:24 +0100

    test: add lexer test case for float token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -16,7 +16,7 @@ var add = func(x, y) {
 var result = add(five, ten);
 !-/*5;
 5 <= 10 >= 5;
-2 > 0;
+2.2 > 0;
 if (5 < 10) {
 	return true;
 } else {
@@ -79,7 +79,7 @@ if (5 < 10) {
 			{token.GE, ">="},
 			{token.INT, "5"},
 			{token.SEMICOLON, ";"},
-			{token.INT, "2"},
+			{token.FLOAT, "2.2"},
 			{token.GT, ">"},
 			{token.INT, "0"},
 			{token.SEMICOLON, ";"},
