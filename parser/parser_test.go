ac22bb0dcd5cf102b26a7844b4313f2af7e5494f
radeqq007 <radeqq007@interia.pl>
2025-01-10 21:59:59 +0100

    test: add test for boolean literal expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -242,6 +242,39 @@ func TestStringLiteralExpression(t *testing.T) {
   }
 }
 
+func TestBooleanLiteralExpression(t *testing.T) {
+  input := "true;"
+  l := lexer.New(input)
+  p := New(l)
+  program := p.ParseProgram()
+
+  checkParserErrors(t, p)
+
+  if len(program.Statements) != 1 {
+    t.Fatalf("program has not enough statements. got=%d", len(program.Statements))
+  }
+
+  stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+ 
+  if !ok {
+    t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+  }
+
+  boolean, ok := stmt.Expression.(*ast.Boolean)
+  
+  if !ok {
+    t.Fatalf("exp not *ast.Boolean. got=%T", stmt.Expression)
+  }
+  
+  if boolean.Value != true {
+    t.Errorf("boolean.Value not %v. got=%v", true, boolean.Value)
+  }
+
+  if boolean.TokenLiteral() != "true" {
+    t.Errorf("boolean.TokenLiteral not %s. got=%s", "true", boolean.TokenLiteral())
+  }
+}
+
 func TestParsingPrefixExpressions(t *testing.T) {
   prefixTests := []struct {
     input    string
