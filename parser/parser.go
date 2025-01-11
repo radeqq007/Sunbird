2d78b633205d212a6acdcae0abf29dcfefb78ea7
radeqq007 <radeqq007@interia.pl>
2025-01-11 14:22:01 +0100

    feat: parsing grouped expressions

--- parser/parser.go
+++ parser/parser.go
@@ -46,6 +46,7 @@ func New(l *lexer.Lexer) *Parser {
   p.registerPrefix(token.MINUS, p.parsePrefixExpression)
  	p.registerPrefix(token.TRUE, p.parseBoolean)
   p.registerPrefix(token.FALSE, p.parseBoolean)
+  p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)	
@@ -203,6 +204,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	}
 
   return leftExp
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+  p.nextToken()
+
+  exp := p.parseExpression(LOWEST)
+
+  if !p.expectPeek(token.RPAREN) {
+    return nil
+  }
+
+  return exp
 }
 
 func (p *Parser) parseIdentifier() ast.Expression {
