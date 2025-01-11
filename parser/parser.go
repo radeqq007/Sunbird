08d96b493be4df00318b48d4e7dd9cdbf77e0651
radeqq007 <radeqq007@interia.pl>
2025-01-11 11:45:18 +0100

    feat: add parse boolean function

--- parser/parser.go
+++ parser/parser.go
@@ -207,6 +207,10 @@ func (p *Parser) parseIdentifier() ast.Expression {
   return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
+func (p *Parser) parseBoolean() ast.Expression {
+  return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
 func (p *Parser) parseIntegerLiteral() ast.Expression {
   lit := &ast.IntegerLiteral{Token: p.curToken}
 
