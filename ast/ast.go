2fe7324dc2b346274944bd27f2a34ff49866ded2
radeqq007 <radeqq007@interia.pl>
2025-02-26 18:49:17 +0100

    feat: add FunctionLiteral to ast

--- ast/ast.go
+++ ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"strings"
 	"vex/token"
 )
 
@@ -114,6 +115,31 @@ func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 func (i *Identifier) String() string { return i.Value }
+
+type FunctionLiteral struct {
+  Token token.Token
+  Parameters []*Identifier
+  Body *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+  var out bytes.Buffer
+
+  params := []string{}
+  for _, p := range fl.Parameters {
+    params = append(params, p.String())
+  }
+
+  out.WriteString(fl.TokenLiteral())
+  out.WriteString("(")
+  out.WriteString(strings.Join(params, ", "))
+  out.WriteString(") ")
+  out.WriteString(fl.Body.String())
+
+  return out.String()
+}
 
 type ReturnStatement struct {
   Token 			token.Token
