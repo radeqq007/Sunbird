0a4cb33a64fbd15df5468934e6bda77df9effdc5
radeqq007 <radeqq007@interia.pl>
2025-02-27 14:22:01 +0100

    feat: add object.Float type

--- object/object.go
+++ object/object.go
@@ -6,6 +6,7 @@ type ObjectType string
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	FLOAT_OBJ = "FLOAT"
 )
 
 type Object interface {
@@ -19,3 +20,10 @@ type Integer struct {
 
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Float struct {
+	Value float64
+}
+
+func (f *Float) Inspect() string { return fmt.Sprintf("%f", f.Value) }
+func (f *Float) Type() ObjectType { return FLOAT_OBJ }
