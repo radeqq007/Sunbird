d53423e8224585140ea657bf36a1f405183a4619
radeqq007 <radeqq007@interia.pl>
2025-02-27 11:40:29 +0100

    feat: parsing function call expressions

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func New(l *lexer.Lexer) *Parser {
   p.registerInfix(token.GT, p.parseInfixExpression)
   p.registerInfix(token.LE, p.parseInfixExpression)
   p.registerInfix(token.GE, p.parseInfixExpression)
+  p.registerInfix(token.LPAREN, p.parseCallExpression)
+
 
   // Read 2 tokens so curToken and peekToken are set
   p.nextToken()
@@ -419,4 +421,39 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
   
 
   return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+  exp := &ast.CallExpression{
+    Token: p.curToken,
+    Function: function,
+    Arguments: p.parseCallArguments(),
+  }
+
+  return exp
+}
+
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+  args := []ast.Expression{}
+  
+  if p.peekTokenIs(token.RPAREN) {
+    p.nextToken()
+    return args
+  }
+
+  p.nextToken()
+  args = append(args, p.parseExpression(LOWEST))
+
+  for p.peekTokenIs(token.COMMA) {
+    p.nextToken()
+    p.nextToken()
+    args = append(args, p.parseExpression(LOWEST))
+  }
+
+  if !p.expectPeek(token.RPAREN) {
+    return nil
+  }
+
+  return args
 }
