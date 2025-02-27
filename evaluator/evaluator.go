067dfdfc2413f5512135a02bb07622c80a1ad644
radeqq007 <radeqq007@interia.pl>
2025-02-27 16:35:58 +0100

    feat: setup evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,28 @@
+package evaluator
+
+import (
+	"vex/ast"
+	"vex/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	}
+	
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	
+	return result
+}
