d357a33572da7916e5b6c34f5a6f97cb3ffa7c96
radeqq007 <radeqq007@interia.pl>
2025-02-27 16:40:31 +0100

    feat: finish the REPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"vex/evaluator"
 	"vex/lexer"
 	"vex/parser"
 )
@@ -32,9 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 			
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, program.String())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
