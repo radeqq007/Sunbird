3068b1f79d8b4e33d1c88196e3610b27d1916e31
radeqq007 <radeqq007@interia.pl>
2025-02-28 11:33:29 +0100

    test: test for evaluating bang operator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -76,3 +76,23 @@ func TestEvalBooleanExpression(t *testing.T) {
 
   return true
 }
+
+func TestBangOperator(t *testing.T) {
+  tests := []struct {
+    input string
+    expected bool
+  } {
+    {"!true", false},
+    {"!false", true},
+    {"!5", false}, 
+    {"!!true", true},
+    {"!!false", false},
+    {"!!5", true},
+  }
+
+  for _, tt := range tests {
+    evaluated := testEval(tt.input)
+    testBooleanObject(t, evaluated, tt.expected)
+  }
+}
+  
