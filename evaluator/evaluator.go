f55515a8fe696da304e693277d4624801bbab871
radeqq007 <radeqq007@interia.pl>
2025-03-02 16:35:02 +0100

    feat: evaluating infix float operations

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -106,6 +106,11 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
 	
+	// TODO: handle string here
+
+	case left.Type() == object.FLOAT_OBJ || right.Type() == object.FLOAT_OBJ:
+		return evalFloatInfixExpression(operator, left, right) 
+	
 	default:
 		return NULL
 	}
@@ -127,4 +132,33 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 	default:
 		return NULL
 }
+}
+
+func evalFloatInfixExpression(operator string, left, right object.Object) object.Object {
+	var leftVal, rightVal float64
+
+	if left.Type() == object.INTEGER_OBJ {
+			leftVal = float64(left.(*object.Integer).Value)
+	} else {
+			leftVal = left.(*object.Float).Value
+	}
+
+	if right.Type() == object.INTEGER_OBJ {
+			rightVal = float64(right.(*object.Integer).Value)
+	} else {
+			rightVal = right.(*object.Float).Value
+	}
+
+	switch operator {
+	case "+":
+			return &object.Float{Value: leftVal + rightVal}
+	case "-":
+			return &object.Float{Value: leftVal - rightVal}
+	case "*":
+			return &object.Float{Value: leftVal * rightVal}
+	case "/":
+			return &object.Float{Value: leftVal / rightVal}
+	default:
+			return NULL
+	}
 }
