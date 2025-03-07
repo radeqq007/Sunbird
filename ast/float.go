b4ca011c15f5632ef7fee00e9e461a42c488a79b
radeqq007 <radeqq007@interia.pl>
2025-03-07 18:29:40 +0100

    refactor(ast): break ast into multiple files

---
+++ ast/float.go
@@ -0,0 +1,12 @@
+package ast
+
+import "sunbird/token"
+
+type FloatLiteral struct {
+	Token token.Token
+	Value float64
+}
+
+func (fl *FloatLiteral) expressionNode()      {}
+func (fl *FloatLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FloatLiteral) String() string       { return fl.Token.Literal }
