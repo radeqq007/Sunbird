3b4005a67c24b5556e4bd193f3f9f21714f2de62
radeqq007 <radeqq007@interia.pl>
2025-03-07 22:18:11 +0100

    fix(repl): pass env argument to Eval function

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"sunbird/evaluator"
 	"sunbird/lexer"
+	"sunbird/object"
 	"sunbird/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = "$ "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 			
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
