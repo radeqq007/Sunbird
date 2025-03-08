002be2eb3518acaf3af9d24b7602a2fc4ff16ee3
radeqq007 <radeqq007@interia.pl>
2025-03-08 09:26:30 +0100

    feat(object): add function object

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"sunbird/ast"
+)
 
 type ObjectType string
 
@@ -9,6 +14,7 @@ const (
 	FLOAT_OBJ = "FLOAT"
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ = "NULL"
+	FUNCTION_OBJ = "FUNCTION"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
 )
@@ -58,3 +64,29 @@ type Error struct {
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string { return "ERROR: " + e.Message }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	
+	params := []string{}
+	
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	
+	out.WriteString("func")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+	
+	return out.String()
+}
