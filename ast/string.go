89f6e91742ea531552d03506fc1f81eb8610759c
radeqq007 <radeqq007@interia.pl>
2025-03-08 20:50:24 +0100

    feat(ast): remove quotes from string literal .String() method

--- ast/string.go
+++ ast/string.go
@@ -9,4 +9,4 @@ type StringLiteral struct {
 
 func (sl *StringLiteral) expressionNode()      {}
 func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
-func (sl *StringLiteral) String() string       { return "\"" + sl.Token.Literal + "\"" }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
