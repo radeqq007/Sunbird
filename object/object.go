86b46de6508343259cc118e4aa46cc1bebb6bbfd
radeqq007 <radeqq007@interia.pl>
2025-03-09 10:20:06 +0100

    feat(object): add builtin function object

--- object/object.go
+++ object/object.go
@@ -18,6 +18,7 @@ const (
 	FUNCTION_OBJ = "FUNCTION"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -98,3 +99,12 @@ func (f *Function) Inspect() string {
 	
 	return out.String()
 }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string { return "builtin function" }
