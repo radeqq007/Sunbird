70d34c733d59317cd76bc304b2a639411fa38420
radeqq007 <radeqq007@interia.pl>
2025-03-09 17:15:30 +0100

    test(evaluator): fix indentation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -429,14 +429,15 @@ func TestArrayIndexExpressions(t *testing.T) {
     { "var myArray = [1, 2, 3]; var i = myArray[0]; myArray[i]", 2, },
     { "[1, 2, 3][3]", nil, },
     { "[1, 2, 3][-1]", 3, },
-}
-for _, tt := range tests {
-evaluated := testEval(tt.input)
-integer, ok := tt.expected.(int)
-if ok {
-testIntegerObject(t, evaluated, int64(integer))
-} else {
-testNullObject(t, evaluated)
-}
-}
+  }
+  for _, tt := range tests {
+    evaluated := testEval(tt.input)
+    integer, ok := tt.expected.(int)
+    
+    if ok {
+      testIntegerObject(t, evaluated, int64(integer))
+    } else {
+      testNullObject(t, evaluated)
+    }
+  }
 }
