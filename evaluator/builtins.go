c32357a28690278eec115126b06ec6d50f387dc8
radeqq007 <radeqq007@interia.pl>
2025-03-09 22:02:57 +0100

    feat(evaluator): handle arrays in len() builtin function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -14,6 +14,9 @@ var builtins = map[string]*object.Builtin{
 				case *object.String:
 					return &object.Integer{ Value: int64(len(arg.Value)) }
 
+				case *object.Array:
+					return &object.Integer{ Value: int64(len(arg.Elements)) }
+
 				default:
 					return newError("argument to `len` not supported, got %s", args[0].Type())
 				}
