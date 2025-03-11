c15a1ae33354d6164922eb119de9bf33f5b23b8b
radeqq007 <radeqq007@interia.pl>
2025-03-11 20:37:21 +0100

    feat(lexer): lexing && and || tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -147,6 +147,20 @@ func (l *Lexer) NextToken() token.Token {
 		} else {
 			tok = newToken(token.GT, l.ch)
 		}
+
+	case '|':
+		if l.peekChar() == '|' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.OR, Literal: string(ch) + string(l.ch)}
+		}
+
+	case '&':
+		if l.peekChar() == '&' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.AND, Literal: string(ch) + string(l.ch)}
+		}
 	
 	case '(':
 			tok = newToken(token.LPAREN, l.ch)
