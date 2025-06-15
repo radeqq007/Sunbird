b5f4a1083cdf984e0deb9b20aa55fa8e75e19df7
radeqq007 <radeqq007@interia.pl>
2025-06-15 14:23:41 +0200

    refactor(token): use an enum for token type

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,6 +1,6 @@
 package token
 
-type TokenType string
+type TokenType uint8
 
 type Token struct {
 	Type    TokenType
@@ -10,61 +10,146 @@ type Token struct {
 }
 
 const (
-	ILLEGAL = "ILLEGAL"
-	EOF     = "EOF" // End of file
+	ILLEGAL TokenType = iota
+	EOF               // End of file
 
 	// Identifiers + literals
-	IDENT  = "IDENT"
-	FLOAT  = "FLOAT"
-	INT    = "INT"
-	STRING = "STRING"
+	IDENT
+	FLOAT
+	INT
+	STRING
 
 	// Operators
-	ASSIGN   = "="
-	PLUS     = "+"
-	MINUS    = "-"
-	BANG     = "!"
-	ASTERISK = "*"
-	SLASH    = "/"
-	PIPE     = "|>"
+	ASSIGN
+	PLUS
+	MINUS
+	BANG
+	ASTERISK
+	SLASH
+	PIPE
 
 	// Comparison operators
-	EQ     = "=="
-	NOT_EQ = "!="
+	EQ
+	NOT_EQ
 
-	LT = "<"
-	GT = ">"
+	LT
+	GT
 
-	LE = "<="
-	GE = ">="
+	LE
+	GE
 
 	// Logical operators
-	OR  = "||"
-	AND = "&&"
+	OR
+	AND
 
 	// Delimeter
-	COMMA     = ","
-	SEMICOLON = ";"
+	COMMA
+	SEMICOLON
 
-	LPAREN   = "("
-	RPAREN   = ")"
-	LBRACE   = "{"
-	RBRACE   = "}"
-	LBRACKET = "["
-	RBRACKET = "]"
+	LPAREN
+	RPAREN
+	LBRACE
+	RBRACE
+	LBRACKET
+	RBRACKET
 
 	// Keywords
-	FUNCTION = "FUNCTION"
-	VAR      = "VAR"
-	TRUE     = "TRUE"
-	FALSE    = "FALSE"
-	IF       = "IF"
-	ELSE     = "ELSE"
-	RETURN   = "RETURN"
-	NULL     = "NULL"
-	FOR      = "FOR"
-	WHILE    = "WHILE"
+	FUNCTION
+	VAR
+	TRUE
+	FALSE
+	IF
+	ELSE
+	RETURN
+	NULL
+	FOR
+	WHILE
 )
+
+func (tt TokenType) String() string {
+	switch tt {
+	case ILLEGAL:
+		return "ILLEGAL"
+	case EOF:
+		return "EOF"
+	case IDENT:
+		return "IDENT"
+	case FLOAT:
+		return "FLOAT"
+	case INT:
+		return "INT"
+	case STRING:
+		return "STRING"
+	case ASSIGN:
+		return "="
+	case PLUS:
+		return "+"
+	case MINUS:
+		return "-"
+	case BANG:
+		return "!"
+	case ASTERISK:
+		return "*"
+	case SLASH:
+		return "/"
+	case PIPE:
+		return "|>"
+	case EQ:
+		return "=="
+	case NOT_EQ:
+		return "!="
+	case LT:
+		return "<"
+	case GT:
+		return ">"
+	case LE:
+		return "<="
+	case GE:
+		return ">="
+	case OR:
+		return "||"
+	case AND:
+		return "&&"
+	case COMMA:
+		return ","
+	case SEMICOLON:
+		return ";"
+	case LPAREN:
+		return "("
+	case RPAREN:
+		return ")"
+	case LBRACE:
+		return "{"
+	case RBRACE:
+		return "}"
+	case LBRACKET:
+		return "["
+	case RBRACKET:
+		return "]"
+	case FUNCTION:
+		return "FUNCTION"
+	case VAR:
+		return "VAR"
+	case TRUE:
+		return "TRUE"
+	case FALSE:
+		return "FALSE"
+	case IF:
+		return "IF"
+	case ELSE:
+		return "ELSE"
+	case RETURN:
+		return "RETURN"
+	case NULL:
+		return "NULL"
+	case FOR:
+		return "FOR"
+	case WHILE:
+		return "WHILE"
+	default:
+		return "UNKNOWN"
+	}
+}
 
 var keywords = map[string]TokenType{
 	"func":   FUNCTION,
