49da215227aa40a464e5cdf7af4b1c0dd4a52d36
radeqq007 <radeqq007@interia.pl>
2025-12-26 17:02:22 +0100

    feat(evaluator): evaluating has index expressions

--- internal/evaluator/index.go
+++ internal/evaluator/index.go
@@ -7,6 +7,9 @@ func evalIndexExpression(left, index object.Object, line, col int) object.Object
 	case left.Type() == object.ArrayObj && index.Type() == object.IntegerObj:
 		return evalArrayIndexExpression(left, index, line, col)
 
+	case left.Type() == object.HashObj:
+		return evalHashIndexExpression(left, index, line, col)
+
 	default:
 		return newError(line, col, "index operator not supported: %s", left.Type().String())
 	}
@@ -30,4 +33,27 @@ func evalArrayIndexExpression(left, index object.Object, line, col int) object.O
 	}
 
 	return array.Elements[idx]
+}
+
+func evalHashIndexExpression(left, index object.Object, line, col int) object.Object {
+	hash, ok := left.(*object.Hash)
+	if !ok {
+		return newError(line, col, "index operator not supported: %s", left.Type())
+	}
+
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return newError(line, col, "unusable as hash key: %s", index.Type())
+	}
+
+	pair, ok := hash.Pairs[key.HashKey()]
+	if ok {
+		return pair.Value
+	}
+
+	if hash.Proto != nil {
+		return evalHashIndexExpression(hash.Proto, index, line, col)
+	}
+
+	return NULL
 }
