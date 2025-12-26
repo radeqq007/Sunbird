137e9eead5a32a3c169563d75843e6516e201487
radeqq007 <radeqq007@interia.pl>
2025-12-26 17:08:53 +0100

    feat(lexer): lexing colon token

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -212,6 +212,9 @@ func (l *Lexer) NextToken() token.Token {
 	case ']':
 		tok = l.newToken(token.RBracket, l.ch)
 
+	case ':':
+		tok = l.newToken(token.Colon, l.ch)
+
 	case '"', '\'':
 		tok.Type = token.String
 		lit, err := l.readString()
