3b0ad77cee8b2f47c12f1963a9ce8a047b3118b9
radeqq007 <radeqq007@interia.pl>
2025-12-28 20:13:14 +0100

    refactor(modules/str): use the errors package for handling errors

--- internal/modules/str/str.go
+++ internal/modules/str/str.go
@@ -2,6 +2,7 @@ package str
 
 import (
 	"strings"
+	"sunbird/internal/errors"
 	"sunbird/internal/modules/modbuilder"
 	"sunbird/internal/object"
 )
@@ -20,139 +21,175 @@ var Module = modbuilder.NewModuleBuilder().
 	Build()
 
 func concat(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
-	
-	str1, ok1 := args[0].(*object.String)
-	str2, ok2 := args[1].(*object.String)
-	if !ok1 || !ok2 {
-		return object.NewError(0, 0, "wrong arguments: expected 2 strings, got %T and %T", args[0].Type(), args[1].Type())
+
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	err = errors.ExpectType(0, 0, args[1], object.StringObj)
+	if err != nil {
+		return err
+	}
+
+	str1 := args[0].(*object.String)
+	str2 := args[1].(*object.String)
+
 	return &object.String{Value: str1.Value + str2.Value}
 }
 
 func isEmpty(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 1, got %d", len(args))
+	err := errors.ExpectNumberOfArguments(0, 0, 1, args)
+	if err != nil {
+		return err
 	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	str := args[0].(*object.String)
+
 	return &object.Boolean{Value: str.Value == ""}
 }
 
 func startsWith(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
-	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
 
-	startStr, ok := args[1].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[1].Type())
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	err = errors.ExpectType(0, 0, args[1], object.StringObj)
+	if err != nil {
+		return err
+	}
+
+	str := args[0].(*object.String)
+	startStr := args[1].(*object.String)
+
 	return &object.Boolean{Value: strings.HasPrefix(str.Value, startStr.Value)}
 }
 
 func endsWith(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
-	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
 
-	endStr, ok := args[1].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[1].Type())
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	err = errors.ExpectType(0, 0, args[1], object.StringObj)
+	if err != nil {
+		return err
+	}
+
+	str := args[0].(*object.String)
+	endStr := args[1].(*object.String)
+
 	return &object.Boolean{Value: strings.HasSuffix(str.Value, endStr.Value)}
 }
 
 func contains(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
-	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
 
-	subStr, ok := args[1].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[1].Type())
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	err = errors.ExpectType(0, 0, args[1], object.StringObj)
+	if err != nil {
+		return err
+	}
+
+	str := args[0].(*object.String)
+	subStr := args[1].(*object.String)
+
 	return &object.Boolean{Value: strings.Contains(str.Value, subStr.Value)}
 }
 
 func toUpper(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 1, got %d", len(args))
+	err := errors.ExpectNumberOfArguments(0, 0, 1, args)
+	if err != nil {
+		return err
 	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	str := args[0].(*object.String)
+
 	return &object.String{Value: strings.ToUpper(str.Value)}
 }
 
 func toLower(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 1, got %d", len(args))
+	err := errors.ExpectNumberOfArguments(0, 0, 1, args)
+	if err != nil {
+		return err
 	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	str := args[0].(*object.String)
+
 	return &object.String{Value: strings.ToLower(str.Value)}
 }
 
 func trim(args ...object.Object) object.Object {
-	if len(args) != 1 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 1, got %d", len(args))
+	err := errors.ExpectNumberOfArguments(0, 0, 1, args)
+	if err != nil {
+		return err
 	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
-	
+
+	str := args[0].(*object.String)
+
 	return &object.String{Value: strings.TrimSpace(str.Value)}
 }
 
 func split(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
-	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
 
-	sep, ok := args[1].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[1].Type())
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
+
+	err = errors.ExpectType(0, 0, args[1], object.StringObj)
+	if err != nil {
+		return err
+	}
+
+	str := args[0].(*object.String)
+	sep := args[1].(*object.String)
 
 	strs := strings.Split(str.Value, sep.Value)
 	var objects []object.Object = make([]object.Object, len(strs))
@@ -164,20 +201,23 @@ func split(args ...object.Object) object.Object {
 }
 
 func repeat(args ...object.Object) object.Object {
-	if len(args) != 2 {
-		return object.NewError(0, 0, "wrong number of arguments: expected 2, got %d", len(args))
-	}
-	
-	str, ok := args[0].(*object.String)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected string, got %T", args[0].Type())
+	err := errors.ExpectNumberOfArguments(0, 0, 2, args)
+	if err != nil {
+		return err
 	}
 
-	count, ok := args[1].(*object.Integer)
-	if !ok {
-		return object.NewError(0, 0, "wrong argument: expected integer, got %T", args[1].Type())
+	err = errors.ExpectType(0, 0, args[0], object.StringObj)
+	if err != nil {
+		return err
 	}
+
+	err = errors.ExpectType(0, 0, args[1], object.IntegerObj)
+	if err != nil {
+		return err
+	}
+
+	str := args[0].(*object.String)
+	count := args[1].(*object.Integer)
 
 	return &object.String{Value: strings.Repeat(str.Value, int(count.Value))}
 }
-
