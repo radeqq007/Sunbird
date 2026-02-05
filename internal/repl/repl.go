d1e1b08a67a0d51333c56c872bd1d59f39fdd46f
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-02-05 12:01:34 +0100

    feat: move from liner to go-prompt for REPL

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -3,94 +3,80 @@ package repl
 import (
 	"io"
 	"os"
-	"path/filepath"
 	"strings"
 	"sunbird/internal/evaluator"
 	"sunbird/internal/lexer"
 	"sunbird/internal/object"
 	"sunbird/internal/parser"
 
-	"github.com/peterh/liner"
+	"github.com/c-bata/go-prompt"
 )
 
 const PROMPT = "$ "
 
-var history_fn = filepath.Join(os.TempDir(), ".sunbird-history")
+var keywords = []prompt.Suggest{
+	{Text: "func", Description: "Define a function"},
+	{Text: "let", Description: "Variable declaration"},
+	{Text: "const", Description: "Variable declaration"},
+	{Text: "true", Description: "Boolean true"},
+	{Text: "false", Description: "Boolean false"},
+	{Text: "if", Description: "Conditional statement"},
+	{Text: "else", Description: "Conditional alternative"},
+	{Text: "return", Description: "Return value"},
+	{Text: "import", Description: "Import a module"},
+	{Text: "as", Description: "Alias for imported module"},
+	{Text: "for", Description: "Loop statement"},
+	{Text: "while", Description: "While loop statement"},
+	{Text: "null", Description: "Null value"},
+	{Text: "break", Description: "Break out of a loop"},
+	{Text: "continue", Description: "Continue to the next iteration of a loop"},
+	{Text: "Int", Description: "Integer type"},
+	{Text: "Float", Description: "Float type"},
+	{Text: "String", Description: "String type"},
+	{Text: "Bool", Description: "Boolean type"},
+	{Text: "Void", Description: "Void type"},
+	{Text: "Array", Description: "Array type"},
+	{Text: "Hash", Description: "Hash type"},
+	{Text: "Func", Description: "Function type"},
+	{Text: "try", Description: "Try block for exception handling"},
+	{Text: "catch", Description: "Catch block for exception handling"},
+	{Text: "finally", Description: "Finally block for exception handling"},
+	{Text: "in", Description: "Iteration keyword"},
+	{Text: "exit", Description: "Exit the REPL"},
+}
 
 func Start(in io.Reader, out io.Writer) {
-	line := liner.NewLiner()
-	defer line.Close()
-
-	line.SetCtrlCAborts(true)
-
-	loadHistory(line)
-
-	setupCompleter(line)
-
 	env := object.NewEnvironment()
 
-	replLoop(line, env, out)
-}
-
-func loadHistory(line *liner.State) {
-	if f, err := os.Open(history_fn); err == nil {
-		_, _ = line.ReadHistory(f)
-
-		_ = f.Close()
-	}
-}
-
-func saveHistory(line *liner.State) {
-	f, _ := os.Create(history_fn)
-	line.WriteHistory(f)
-	_ = f.Close()
-}
-
-func setupCompleter(line *liner.State) {
-	keywords := []string{
-		"func",
-		"let",
-		"true",
-		"false",
-		"if",
-		"else",
-		"return",
-		"null",
-		"import",
-		"as",
-		"for",
-	}
-
-	line.SetCompleter(func(input string) []string {
-		var completions []string
-		for _, keyword := range keywords {
-			if strings.HasPrefix(keyword, input) {
-				completions = append(completions, keyword)
-			}
-		}
-		return completions
-	})
-}
-
-func replLoop(line *liner.State, env *object.Environment, out io.Writer) {
-	for {
-		input, err := line.Prompt(PROMPT)
-		if err != nil && err == liner.ErrPromptAborted {
-			saveHistory(line)
-		}
-
-		if input == "exit" {
-			f, _ := os.Create(history_fn)
-
-			_, _ = line.WriteHistory(f)
-			_ = f.Close()
+	executor := func(input string) {
+		input = strings.TrimSpace(input)
+		if input == "" {
 			return
 		}
 
-		line.AppendHistory(input)
+		if input == "exit" {
+			os.Exit(0)
+		}
 
 		evalInput(input, env, out)
 	}
+
+	completer := func(d prompt.Document) []prompt.Suggest {
+		return prompt.FilterHasPrefix(keywords, d.GetWordBeforeCursor(), true)
+	}
+
+	// highlight := func(input string) []prompt. {
+		// return sunbirdLexer(input)
+	// }
+
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionPrefix("$ "),
+		prompt.OptionTitle("sunbird-repl"),
+	)
+
+	p.Run()
 }
 
 func evalInput(input string, env *object.Environment, out io.Writer) {
