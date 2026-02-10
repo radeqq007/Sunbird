6a47e708117a7b95b7cc7e3a6bb8230e7e858a03
Radosław Kaczmarczyk <radeqq007@interia.pl>
2026-02-10 12:40:10 +0100

    chore: mention Range type in SimpleType comment

--- internal/ast/ast.go
+++ internal/ast/ast.go
@@ -1,40 +1,82 @@
 package ast
 
-import "bytes"
+import (
+	"bytes"
+	"strings"
+	"sunbird/internal/token"
+)
 
-type Node interface {
-	TokenLiteral() string
-	String() string
-}
-
-type Statement interface {
+type TypeAnnotation interface {
 	Node
-	statementNode()
+	typeAnnotation()
 }
 
-type Expression interface {
-	Node
-	expressionNode()
+// SimpleType is for strings, ints, bools, Range type etc.
+type SimpleType struct {
+	Token token.Token
+	Name  string
 }
 
-type Program struct {
-	Statements []Statement
+func (st *SimpleType) typeAnnotation()      {}
+func (st *SimpleType) TokenLiteral() string { return st.Token.Literal }
+func (st *SimpleType) String() string       { return st.Name }
+
+type ArrayType struct {
+	Token       token.Token
+	ElementType TypeAnnotation
 }
 
-func (p *Program) TokenLiteral() string {
-	if len(p.Statements) > 0 {
-		return p.Statements[0].TokenLiteral()
-	}
-
-	return ""
+func (at *ArrayType) typeAnnotation()      {}
+func (at *ArrayType) TokenLiteral() string { return at.Token.Literal }
+func (at *ArrayType) String() string {
+	// return "[" +  "]" + at.ElementType.String()
+	return "Array"
 }
 
-func (p *Program) String() string {
+type HashType struct {
+	Token token.Token
+}
+
+func (ht *HashType) typeAnnotation()      {}
+func (ht *HashType) TokenLiteral() string { return ht.Token.Literal }
+func (ht *HashType) String() string       { return "Hash" }
+
+type FunctionType struct {
+	Token      token.Token
+	Parameters []TypeAnnotation
+	ReturnType TypeAnnotation
+}
+
+func (ft *FunctionType) typeAnnotation()      {}
+func (ft *FunctionType) TokenLiteral() string { return ft.Token.Literal }
+func (ft *FunctionType) String() string {
 	var out bytes.Buffer
 
-	for _, s := range p.Statements {
-		out.WriteString(s.String())
+	params := []string{}
+	for _, p := range ft.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+
+	if ft.ReturnType != nil {
+		out.WriteString(": ")
+		out.WriteString(ft.ReturnType.String())
 	}
 
 	return out.String()
+}
+
+// OptionalType is for optional types like int?
+type OptionalType struct {
+	Token    token.Token
+	BaseType TypeAnnotation
+}
+
+func (ot *OptionalType) typeAnnotation()      {}
+func (ot *OptionalType) TokenLiteral() string { return ot.Token.Literal }
+func (ot *OptionalType) String() string {
+	return ot.BaseType.String() + "?"
 }
