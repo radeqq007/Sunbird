363ff96e355a9454272daec27f2ac1f2e773ff4c
Radosław Kaczmarczyk <radeqq007@interia.pl>
2026-02-11 14:50:23 +0100

    wip(evaluator): use unified value interface for values inside builtins.go

--- internal/evaluator/builtins.go
+++ internal/evaluator/builtins.go
@@ -9,203 +9,222 @@ import (
 
 var builtins = map[string]*object.Builtin{
 	"len": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			err = errors.ExpectOneOfTypes(0, 0, args[0], object.StringObj, object.ArrayObj)
-			if err != nil {
+			err = errors.ExpectOneOfTypes(0, 0, args[0], object.StringKind, object.ArrayKind)
+			if err.IsNull() {
 				return err
 			}
 
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
+			// switch arg := args[0].(type) {
+			// case *object.String:
+			// 	return &object.Integer{Value: int64(len(arg.Value))}
 
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
+			// case *object.Array:
+			// 	return &object.Integer{Value: int64(len(arg.Elements))}
+			// }
+
+			arg := args[0]
+			if arg.IsString() {
+				return object.NewInt(int64(len(arg.AsString().Value)))
 			}
 
-			return nil
+			if arg.IsArray() {
+				return object.NewInt(int64(len(arg.AsArray().Elements)))
+			}
+
+			return NULL
 		},
 	},
 
 	"append": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 2, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			err = errors.ExpectType(0, 0, args[0], object.ArrayObj)
-			if err != nil {
+			err = errors.ExpectType(0, 0, args[0], object.ArrayKind)
+			if !err.IsNull() {
 				return err
 			}
 
-			arr, _ := args[0].(*object.Array)
+			arr := args[0].AsArray()
 
 			newElements := append(arr.Elements, args[1:]...)
 
-			return &object.Array{Elements: newElements}
+			return object.NewArray(newElements)
 		},
 	},
 
 	"type": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			return &object.String{Value: args[0].Type().String()}
+			return object.NewString(args[0].Kind().String())
 		},
 	},
 
 	"string": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			if s, ok := args[0].(*object.String); ok {
-				return s
+			if args[0].IsString() {
+				val := args[0].AsString().Value
+				return object.NewString(val)
 			}
 
-			return &object.String{Value: args[0].Inspect()}
+			return object.NewString(args[0].Inspect())
 		},
 	},
 
 	"int": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			switch arg := args[0].(type) {
-			case *object.Integer:
-				return arg
+			switch args[0].Kind() {
+			case object.IntKind:
+				return args[0]
 
-			case *object.Float:
-				return &object.Integer{Value: int64(arg.Value)}
+			case object.FloatKind:
+				arg := args[0].AsFloat()
+				return object.NewInt(int64(arg))
 
-			case *object.String:
-				num, err := strconv.Atoi(arg.Value)
+			case object.StringKind:
+				arg := args[0].AsString().Value
+				num, err := strconv.Atoi(arg)
 				if err != nil {
-					return errors.NewTypeError(0, 0, "failed to convert string to int: %s", arg.Value)
+					return errors.NewTypeError(0, 0, "failed to convert string to int: %s", arg)
 				}
-				return &object.Integer{Value: int64(num)}
+				return object.NewInt(int64(num))
 
-			case *object.Boolean:
-				if arg.Value {
-					return &object.Integer{Value: 1}
+			case object.BoolKind:
+				arg := args[0].AsBool()
+				if arg {
+					return object.NewInt(1)
 				}
-				return &object.Integer{Value: 0}
+				return object.NewInt(0)
 
 			default:
-				return errors.NewTypeError(0, 0, "argument to `int` not supported, got %s", args[0].Type().String())
+				return errors.NewTypeError(0, 0, "argument to `int` not supported, got %s", args[0].Kind().String())
 			}
 		},
 	},
 
 	"float": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			switch arg := args[0].(type) {
-			case *object.Integer:
-				return &object.Float{Value: float64(arg.Value)}
+			switch args[0].Kind() {
+			case object.IntKind:
+				arg := args[0].AsInt()
+				return object.NewFloat(float64(arg))
 
-			case *object.Float:
-				return arg
+			case object.FloatKind:
+				return args[0]
 
-			case *object.String:
-				num, err := strconv.ParseFloat(arg.Value, 64)
+			case object.StringKind:
+				arg := args[0].AsString().Value
+				num, err := strconv.ParseFloat(arg, 64)
 				if err != nil {
-					return errors.NewTypeError(0, 0, "failed to convert string to float: %s", arg.Value)
+					return errors.NewTypeError(0, 0, "failed to convert string to float: %s", arg)
 				}
-				return &object.Float{Value: num}
+				return object.NewFloat(num)
 
-			case *object.Boolean:
-				if arg.Value {
-					return &object.Float{Value: 1.0}
+			case object.BoolKind:
+				arg := args[0].AsBool()
+				if arg {
+					return object.NewFloat(1.0)
 				}
-				return &object.Float{Value: 0.0}
+				return object.NewFloat(0.0)
 
 			default:
-				return errors.NewTypeError(0, 0, "argument to `float` not supported, got %s", args[0].Type().String())
+				return errors.NewTypeError(0, 0, "argument to `float` not supported, got %s", args[0].Kind().String())
 			}
 		},
 	},
 
 	"bool": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			switch arg := args[0].(type) {
-			case *object.Integer:
-				if arg.Value == 0 {
-					return &object.Boolean{Value: false}
+			switch args[0].Kind() {
+			case object.IntKind:
+				arg := args[0].AsInt()
+				if arg == 0 {
+					return object.NewBool(false)
 				}
-				return &object.Boolean{Value: true}
+				return object.NewBool(true)
 
-			case *object.Float:
-				if arg.Value == 0.0 {
-					return &object.Boolean{Value: false}
+			case object.FloatKind:
+				arg := args[0].AsFloat()
+				if arg == 0.0 {
+					return object.NewBool(false)
 				}
-				return &object.Boolean{Value: true}
+				return object.NewBool(true)
 
-			case *object.String:
-				if arg.Value == "" {
-					return &object.Boolean{Value: false}
+			case object.StringKind:
+				arg := args[0].AsString().Value
+				if arg == "" {
+					return object.NewBool(false)
 				}
-				return &object.Boolean{Value: true}
+				return object.NewBool(true)
 
-			case *object.Boolean:
-				return arg
+			case object.BoolKind:
+				return args[0]
 
 			default:
-				return errors.NewTypeError(0, 0, "argument to `bool` not supported, got %s", args[0].Type().String())
+				return errors.NewTypeError(0, 0, "argument to `bool` not supported, got %s", args[0].Kind().String())
 			}
 		},
 	},
 
 	"exit": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 0, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
 			os.Exit(0)
-			return nil
+			return object.NewNull()
 		},
 	},
 
 	"error": {
-		Fn: func(args ...object.Object) object.Object {
+		Fn: func(args ...object.Value) object.Value {
 			err := errors.ExpectNumberOfArguments(0, 0, 1, args)
-			if err != nil {
+			if !err.IsNull() {
 				return err
 			}
 
-			err = errors.ExpectType(0, 0, args[0], object.StringObj)
-			if err != nil {
+			err = errors.ExpectType(0, 0, args[0], object.StringKind)
+			if !err.IsNull() {
 				return err
 			}
 
 			var msg string
-			if s, ok := args[0].(*object.String); ok {
-				msg = s.Value
+			if args[0].IsString() {
+				msg = args[0].AsString().Value
 			} else {
 				msg = args[0].Inspect()
 			}
